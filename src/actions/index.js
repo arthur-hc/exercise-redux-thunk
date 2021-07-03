3317d424d28ea83a9825767f119fdb51df875580
Arthur Hermann <arthurhermannc@gmail.com>
2021-07-03 11:27:54 -0300

    cat request

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,25 @@ export const requestAPI = () => ({ type: REQUEST_API });
 
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
-export function fetchAPI() {
+// export function fetchAPI() {
+//   // Desenvolva aqui o código da action assíncrona
+//   return (dispatch) => {
+//     dispatch(requestAPI());
+//     return fetch('https://aws.random.cat/meow')
+//       .then((r) => r.json()
+//         .then(
+//           (json) => dispatch(getPicture(json))
+//         ));
+//   };
+// };
+
+// COM ASYNC
+export const fetchAPI = () => {
   // Desenvolva aqui o código da action assíncrona
-}
+  return async (dispatch) => {
+    dispatch(requestAPI());
+    const reponse = await fetch('https://aws.random.cat/meow')
+    const json =  await reponse.json();
+    dispatch(getPicture(json));
+  };
+};
